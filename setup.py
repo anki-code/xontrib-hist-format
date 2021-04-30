0db399ec024c2e868abc120e13706d0a3bd1fbc0
Andy Kipp <anki-code@users.noreply.github.com>
2021-04-30 17:48:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setuptools.setup(
     name='xontrib-hist-format',
-    version='0.0.10',
+    version='0.0.11',
     license='MIT',
     author='anki-code',
     author_email='no@no.no',
